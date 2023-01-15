a30e1de0414a45b7a6ac774540a9c01e6bc9df27
test <test@gmail.com>
2023-01-15 20:41:52 +0200

    first solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,42 @@
 from django.db import models
+
+
+class Race(models.Model):
+    RACES = (
+        ("Elf", "elf"),
+        ("Dwarf", "dwarf"),
+        ("Human", "human"),
+        ("Ork", "ork"),
+    )
+    name = models.CharField(
+        choices=RACES,
+        default="BB",
+        max_length=255,
+        unique=True
+    )
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(
+        "This field describes what kind of bonus players can get from it.",
+        max_length=255,
+    )
+
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
--- main.py
+++ main.py
@@ -1,10 +1,68 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+
+    races = Race.objects
+    skills = Skill.objects
+    guilds = Guild.objects
+    players = Player.objects
+
+    races.all().delete()
+    skills.all().delete()
+    guilds.all().delete()
+    players.all().delete()
+
+    for name, attributes in json.load(open("players.json", "r")).items():
+        race_name = attributes["race"]["name"]
+        race_description = attributes["race"]["description"]
+        race_skills = attributes["race"]["skills"]
+
+        try:
+            guild_name = attributes["guild"]["name"]
+            guild_description = attributes["guild"]["description"]
+        except TypeError:
+            pass
+
+        if races.filter(name=race_name).exists() is False:
+            races.create(
+                name=race_name,
+                description=race_description
+            )
+
+        if bool(attributes["guild"]) is True:
+            if guilds.filter(name=guild_name).exists() is False:
+
+                guilds.create(
+                    name=guild_name,
+                    description=guild_description
+                )
+
+        if bool(race_skills) is True:
+            for skill in race_skills:
+                if skills.filter(name=skill["name"]).exists() is False:
+
+                    skills.create(
+                        name=skill["name"],
+                        bonus=skill["bonus"],
+                        race=races.get(name=race_name)
+                    )
+
+        players.create(
+            nickname=name,
+            email=attributes["email"],
+            bio=attributes["bio"],
+            race=races.get(name=race_name),
+        )
+
+        if bool(attributes["guild"]) is True:
+            players.filter(nickname=name).update(
+                guild=guilds.get(name=guild_name)
+            )
 
 
 if __name__ == "__main__":
